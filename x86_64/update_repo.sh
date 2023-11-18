894cbaa5300f2df735f3c8fd8618121b5ee5a6b9
D3vil0p3r <crazysnob@live.it>
2023-11-18 23:49:53 +0100

    Update update_repo.sh

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-REPO_NAME="athena-repository"
+REPO_NAME="athena"
 
-rm $REPO_NAME*
+rm *.db *.db.sig *.db.tar.gz *.db.tar.gz.sig *.files *.files.sig *.files.tar.gz *.files.tar.gz.sig
 
 repo-add --verify --sign -R $REPO_NAME.db.tar.gz *.pkg.tar.zst
+repo-add --verify --sign -R $REPO_NAME-repository.db.tar.gz *.pkg.tar.zst
 
 echo "####################################"
 echo "Repo Updated!!"
