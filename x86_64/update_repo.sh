249fc04c2c09cd1bb42e80407979ede7220ffc10
D3vil0p3r <>
2023-11-22 20:48:24 +0100

    Keep active connection after OS installation

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -5,7 +5,6 @@ REPO_NAME="athena"
 rm *.db *.db.sig *.db.tar.gz *.db.tar.gz.sig *.files *.files.sig *.files.tar.gz *.files.tar.gz.sig
 
 repo-add --verify --sign -R $REPO_NAME.db.tar.gz *.pkg.tar.zst
-repo-add --verify --sign -R $REPO_NAME-repository.db.tar.gz *.pkg.tar.zst
 
 echo "####################################"
 echo "Repo Updated!!"
